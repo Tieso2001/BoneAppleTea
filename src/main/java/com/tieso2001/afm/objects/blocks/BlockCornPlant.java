3a45e195c0f8c366e071aa5681a0aa42827276ad
Ties <tiesoosthoek@gmail.com>
2018-11-12 22:55:31 +0100

    removed corn plant item registry & disabled right click harvesting

--- src/main/java/com/tieso2001/afm/objects/blocks/BlockCornPlant.java
+++ src/main/java/com/tieso2001/afm/objects/blocks/BlockCornPlant.java
@@ -4,17 +4,10 @@ import com.tieso2001.afm.init.BlockInit;
 import com.tieso2001.afm.init.ItemInit;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public class BlockCornPlant extends BlockCrops {
 
@@ -35,9 +28,9 @@ public class BlockCornPlant extends BlockCrops {
         setRegistryName(name);
 
         BlockInit.BLOCKS.add(this);
-        ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 
+    /*
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         if (worldIn.isRemote) {
@@ -49,6 +42,7 @@ public class BlockCornPlant extends BlockCrops {
         }
         return false;
     }
+    */
 
     @Override
     protected Item getSeed() {
