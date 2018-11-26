545f00c4f580137141926a61e3bf6d18198158be
Bas van den Boom <basvandenboom3@gmail.com>
2018-11-26 20:22:11 +0100

    Added Yeast class

---
+++ src/main/java/com/tieso2001/afm/objects/items/tools/ItemYeast.java
@@ -0,0 +1,25 @@
+package com.tieso2001.afm.objects.items.tools;
+
+import com.tieso2001.afm.Main;
+import com.tieso2001.afm.init.ItemInit;
+import com.tieso2001.afm.util.IHasModel;
+import net.minecraft.item.Item;
+
+public class ItemYeast extends Item implements IHasModel {
+
+    public ItemYeast(String name) {
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(Main.afmtab);
+
+        setMaxStackSize(1);
+        setMaxDamage(15);
+
+        ItemInit.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+}
