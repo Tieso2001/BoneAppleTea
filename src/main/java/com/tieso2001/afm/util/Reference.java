795e612ac231658a886d95cbd7d6daf4130e84bc
Ties <tiesoosthoek@gmail.com>
2018-11-26 15:55:18 +0100

    renamed mod

--- src/main/java/com/tieso2001/afm/util/Reference.java
+++ src/main/java/com/tieso2001/afm/util/Reference.java
@@ -3,7 +3,7 @@ package com.tieso2001.afm.util;
 public class Reference {
 
     public static final String MOD_ID = "afm";
-    public static final String NAME = "Awesome Farm Mod";
+    public static final String NAME = "AwesomeFarmMod";
     public static final String VERSION = "1.0.0a1";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "com.tieso2001.afm.proxy.ClientProxy";
