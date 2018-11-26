ab94f5eeb0cd017349782f8110e827f030bb0782
Bas van den Boom <basvandenboom3@gmail.com>
2018-11-26 18:55:47 +0100

    Created mortar item class

---
+++ src/main/java/com/tieso2001/afm/objects/items/tools/ItemMortar.java
@@ -0,0 +1,39 @@
+package com.tieso2001.afm.objects.items.tools;
+
+import com.tieso2001.afm.Main;
+import com.tieso2001.afm.util.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Set;
+
+public class ItemMortar extends ItemTool implements IHasModel {
+
+    public ItemMortar(float attackDamageIn, float attackSpeedIn, ToolMaterial materialIn, Set<Block> effectiveBlocksIn) {
+        super(attackDamageIn, attackSpeedIn, materialIn, effectiveBlocksIn);
+    }
+
+    public ItemMortar(ToolMaterial materialIn, Set<Block> effectiveBlocksIn) {
+        super(materialIn, effectiveBlocksIn);
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        ItemStack stack = player.getHeldItem(hand);
+        IBlockState state = worldIn.getBlockState(pos);
+        return EnumActionResult.SUCCESS;
+    }
+}
