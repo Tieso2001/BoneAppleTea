bef6bf268825d5b2ac09d4d48bf98c3b69f96f65
Ties <tiesoosthoek@gmail.com>
2018-11-26 15:46:55 +0100

    changed version to 1.0.0a1

--- src/main/java/com/tieso2001/afm/util/Reference.java
+++ src/main/java/com/tieso2001/afm/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "afm";
     public static final String NAME = "Awesome Farm Mod";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0a1";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "com.tieso2001.afm.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.tieso2001.afm.proxy.CommonProxy";
