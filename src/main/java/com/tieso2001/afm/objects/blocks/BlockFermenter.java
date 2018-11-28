ff9ee95167192ea494e122a7d69c628cd7f036f4
Ties <tiesoosthoek@gmail.com>
2018-11-28 21:40:43 +0100

    Made fermenter rotatable

--- src/main/java/com/tieso2001/afm/objects/blocks/BlockFermenter.java
+++ src/main/java/com/tieso2001/afm/objects/blocks/BlockFermenter.java
@@ -7,24 +7,52 @@ import com.tieso2001.afm.util.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 public class BlockFermenter extends Block implements IHasModel, ITileEntityProvider {
 
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
+
     public BlockFermenter(String name, Material material) {
         super(material);
         setUnlocalizedName(name);
         setRegistryName(name);
         setCreativeTab(Main.afmtab);
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 
         BlockInit.BLOCKS.add(this);
         ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING);
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
     }
 
     @Override
