69004a7c83521580ea9e0366fdf29f07af95ad4c
Bas van den Boom <basvandenboom3@gmail.com>
2019-01-03 21:54:08 +0100

    Refactored code

--- src/main/java/com/tieso2001/boneappletea/object/items/food/ItemGoldenFrikandelRoll.java
+++ src/main/java/com/tieso2001/boneappletea/object/items/food/ItemGoldenFrikandelRoll.java
@@ -1,18 +1,10 @@
 package com.tieso2001.boneappletea.object.items.food;
 
-import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.MobEffects;
-import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.stats.StatList;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
 public class ItemGoldenFrikandelRoll extends ItemFood {
@@ -23,42 +15,14 @@ public class ItemGoldenFrikandelRoll extends ItemFood {
         setRegistryName(name);
     }
 
-    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving) {
-
+    @Override
+    protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
         if (!worldIn.isRemote) {
-            entityLiving.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 60 * 20, 2));
-            entityLiving.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 30 * 20, 4));
-            entityLiving.heal(entityLiving.getMaxHealth());
+            player.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 60 * 20, 2));
+            player.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 30 * 20, 4));
+            player.heal(player.getMaxHealth());
         }
 
-        if (entityLiving instanceof EntityPlayerMP) {
-            EntityPlayerMP entityplayermp = (EntityPlayerMP) entityLiving;
-            CriteriaTriggers.CONSUME_ITEM.trigger(entityplayermp, stack);
-            entityplayermp.addStat(StatList.getObjectUseStats(this));
-        }
-
-        if (entityLiving instanceof EntityPlayer && !((EntityPlayer) entityLiving).capabilities.isCreativeMode) {
-            stack.shrink(1);
-        }
-
-        return stack.isEmpty() ? ItemStack.EMPTY : stack;
-
+        super.onFoodEaten(stack, worldIn, player);
     }
-
-    public int getMaxItemUseDuration(ItemStack stack) {
-        return 32;
-    }
-
-    public EnumAction getItemUseAction(ItemStack stack) {
-        return EnumAction.EAT;
-    }
-
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-        if (playerIn.isCreative()) {
-            return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
-        }
-        playerIn.setActiveHand(handIn);
-        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
-    }
-
 }
