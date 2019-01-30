1c4126adb09fcfce5ecfa411a4fd87a4ebbe0b47
Ties <tiesoosthoek@gmail.com>
2019-01-30 17:21:26 +0100

    Changed grass seeds values 1 -> 2

--- src/main/java/com/tieso2001/boneappletea/world/GrassSeeds.java
+++ src/main/java/com/tieso2001/boneappletea/world/GrassSeeds.java
@@ -8,11 +8,11 @@ import net.minecraftforge.common.MinecraftForge;
 public class GrassSeeds {
 
     public static void register() {
-        addGrassSeed(ModItems.BARLEY_SEEDS, 1);
-        addGrassSeed(ModItems.CORN_KERNELS, 1);
+        addGrassSeed(ModItems.BARLEY_SEEDS, 2);
+        addGrassSeed(ModItems.CORN_KERNELS, 2);
     }
 
-    public static void addGrassSeed(Item seed, int weight) {
+    private static void addGrassSeed(Item seed, int weight) {
         MinecraftForge.addGrassSeed(new ItemStack(seed), weight);
     }
 
