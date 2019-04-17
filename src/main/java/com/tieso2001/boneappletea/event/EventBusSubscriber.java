474ce0b1dac79dcd8ec8010bce9edf92a129f47d
Ties <tiesoosthoek@gmail.com>
2019-04-17 23:02:21 +0200

    Improved bits of code

--- src/main/java/com/tieso2001/boneappletea/event/EventBusSubscriber.java
+++ src/main/java/com/tieso2001/boneappletea/event/EventBusSubscriber.java
@@ -1,22 +1,25 @@
 package com.tieso2001.boneappletea.event;
 
 import com.tieso2001.boneappletea.BoneAppleTea;
+import com.tieso2001.boneappletea.block.BlockCorn;
 import com.tieso2001.boneappletea.init.ModBlocks;
-import com.tieso2001.boneappletea.init.ModItems;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemSeedFood;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = BoneAppleTea.MODID)
-public class EventSubscriber
+public class EventBusSubscriber
 {
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
         final Block[] blocks = {
-                ModBlocks.CORN
+                new BlockCorn().setRegistryName("corn").setTranslationKey("corn")
         };
 
         event.getRegistry().registerAll(blocks);
@@ -26,10 +29,10 @@ public class EventSubscriber
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
         final Item[] items = {
-                ModItems.CORN,
-                ModItems.CORN_KERNELS,
-                ModItems.POPCORN,
-                ModItems.ROASTED_CORN
+                new ItemFood(1, 0.6F,false).setRegistryName("corn").setTranslationKey("corn").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
+                new ItemSeedFood(1, 0.3F, ModBlocks.CORN, Blocks.FARMLAND).setRegistryName("corn_kernels").setTranslationKey("corn_kernels").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
+                new ItemFood(2, 0.6F, false).setRegistryName("popcorn").setTranslationKey("popcorn").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
+                new ItemFood(4, 0.6F, false).setRegistryName("roasted_corn").setTranslationKey("roasted_corn").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA)
         };
 
         final Item[] itemBlocks = {
--- src/main/java/com/tieso2001/boneappletea/init/ModItems.java
+++ src/main/java/com/tieso2001/boneappletea/init/ModItems.java
@@ -1,16 +1,15 @@
 package com.tieso2001.boneappletea.init;
 
 import com.tieso2001.boneappletea.BoneAppleTea;
-import com.tieso2001.boneappletea.item.ItemCornSeeds;
-import com.tieso2001.boneappletea.item.ItemEdible;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemSeedFood;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @GameRegistry.ObjectHolder(BoneAppleTea.MODID)
 public class ModItems
 {
-    public static Item CORN = new ItemEdible("corn",1, false);
-    public static Item CORN_KERNELS = new ItemCornSeeds("corn_kernels",1, false);
-    public static Item POPCORN = new ItemEdible("popcorn",2, false);
-    public static Item ROASTED_CORN = new ItemEdible("roasted_corn",4, false);
+    public static final ItemFood CORN = null;
+    public static final ItemSeedFood CORN_KERNELS = null;
+    public static final ItemFood POPCORN = null;
+    public static final ItemFood ROASTED_CORN = null;
 }
