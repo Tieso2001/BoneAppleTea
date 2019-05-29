f5aaf0801fc2e1ada192c78c28f308881fbd518f
Ties <tiesoosthoek@gmail.com>
2019-05-29 15:07:21 +0200

    Added barley

--- src/main/java/com/tieso2001/boneappletea/event/EventBusSubscriber.java
+++ src/main/java/com/tieso2001/boneappletea/event/EventBusSubscriber.java
@@ -1,16 +1,15 @@
 package com.tieso2001.boneappletea.event;
 
 import com.tieso2001.boneappletea.BoneAppleTea;
+import com.tieso2001.boneappletea.block.BlockBarley;
 import com.tieso2001.boneappletea.block.BlockCorn;
 import com.tieso2001.boneappletea.block.BlockStockPot;
 import com.tieso2001.boneappletea.init.ModBlocks;
 import com.tieso2001.boneappletea.tile.TileStockPot;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockCrops;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemSeedFood;
+import net.minecraft.item.*;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -23,6 +22,7 @@ public class EventBusSubscriber
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
         final Block[] blocks = {
+                new BlockBarley().setRegistryName("barley").setTranslationKey(BoneAppleTea.MODID + "." + "barley"),
                 new BlockCorn().setRegistryName("corn").setTranslationKey(BoneAppleTea.MODID + "." + "corn"),
                 new BlockStockPot().setRegistryName("stock_pot").setTranslationKey(BoneAppleTea.MODID + "." + "stock_pot").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA)
         };
@@ -35,6 +35,9 @@ public class EventBusSubscriber
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
         final Item[] items = {
+                new Item().setRegistryName("barley").setTranslationKey(BoneAppleTea.MODID + "." + "barley").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
+                new Item().setRegistryName("barley_grains").setTranslationKey(BoneAppleTea.MODID + "." + "barley_grains").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
+                new ItemSeeds(ModBlocks.BARLEY, Blocks.FARMLAND).setRegistryName("barley_seeds").setTranslationKey(BoneAppleTea.MODID + "." + "barley_seeds").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
                 new ItemFood(1, 0.6F,false).setRegistryName("corn").setTranslationKey(BoneAppleTea.MODID + "." + "corn").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
                 new ItemSeedFood(1, 0.3F, ModBlocks.CORN, Blocks.FARMLAND).setRegistryName("corn_kernels").setTranslationKey(BoneAppleTea.MODID + "." + "corn_kernels").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
                 new ItemFood(2, 0.6F, false).setRegistryName("popcorn").setTranslationKey(BoneAppleTea.MODID + "." + "popcorn").setCreativeTab(BoneAppleTea.TAB_BONE_APPLE_TEA),
--- src/main/java/com/tieso2001/boneappletea/init/ModBlocks.java
+++ src/main/java/com/tieso2001/boneappletea/init/ModBlocks.java
@@ -3,11 +3,13 @@ package com.tieso2001.boneappletea.init;
 import com.tieso2001.boneappletea.BoneAppleTea;
 import com.tieso2001.boneappletea.block.BlockStockPot;
 import com.tieso2001.boneappletea.block.BlockTallCrops;
+import net.minecraft.block.BlockCrops;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @GameRegistry.ObjectHolder(BoneAppleTea.MODID)
 public class ModBlocks
 {
+    public static final BlockCrops BARLEY = null;
     public static final BlockTallCrops CORN = null;
     public static final BlockStockPot STOCK_POT = null;
 }
